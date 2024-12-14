08617c4e0598c98d9b4c57518277d1f24ab6e100
Priyanshu498 <priyanshu1912003@gmail.com>
2024-12-14 12:36:44 +0530

    Create sonarPipeline.groovy

---
+++ vars/sonarPipeline.groovy
@@ -0,0 +1,80 @@
+def call(Map config = [:]) {
+    pipeline {
+        agent any
+
+        environment {
+            SONARQUBE_ENV = config.sonarEnv ?: 'SonarQube'
+        }
+
+        stages {
+            stage('Clone Repository') {
+                steps {
+                    script {
+                        sh "git clone ${config.repoUrl}"
+                    }
+                }
+            }
+
+            stage('SonarQube Analysis') {
+                steps {
+                    script {
+                        def scannerHome = tool(name: 'sonar', type: 'hudson.plugins.sonar.SonarRunnerInstallation')
+                        withSonarQubeEnv(SONARQUBE_ENV) {
+                            sh """
+                            ${scannerHome}/bin/sonar-scanner \
+                            -Dsonar.projectKey=${config.projectKey} \
+                            -Dsonar.sources=${config.sources ?: '.'} \
+                            -Dsonar.host.url=${SONAR_HOST_URL} \
+                            -Dsonar.login=${SONAR_AUTH_TOKEN}
+                            """
+                        }
+                    }
+                }
+            }
+
+            stage('Build Status') {
+                steps {
+                    script {
+                        if (currentBuild.currentResult == 'SUCCESS') {
+                            echo 'SonarQube analysis completed successfully!'
+                        } else {
+                            echo 'SonarQube analysis encountered issues.'
+                        }
+                    }
+                }
+            }
+
+            stage('Cleanup Workspace') {
+                steps {
+                    cleanWs()
+                }
+            }
+        }
+
+        post {
+            success {
+                script {
+                    slackSend(
+                        channel: '#jenkinnotify',
+                        message: "Build SUCCESS: ${env.JOB_NAME} #${env.BUILD_NUMBER}",
+                        color: "good"
+                    )
+                }
+            }
+            failure {
+                script {
+                    slackSend(
+                        channel: '#jenkinnotify',
+                        message: "Build FAILURE: ${env.JOB_NAME} #${env.BUILD_NUMBER}",
+                        color: "danger"
+                    )
+                }
+            }
+            always {
+                script {
+                    echo "Build finished with status: ${currentBuild.currentResult}"
+                }
+            }
+        }
+    }
+}
